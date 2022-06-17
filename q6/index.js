997ff5d60f459b2c65a08a836f83ca29252581aa
kianika <mona_yaghoubi@gmail.com>
2022-06-17 08:56:16 +0200

    v2

--- q6/index.js
+++ q6/index.js
@@ -1,8 +1,15 @@
-let elem = document.getElementById('ElemId')
-elem.addEventListener("click", function(e){
-e.target.innerHTML = "goodby";
-})
 
+// catch on document...
+document.addEventListener("goodbye", function(event) { 
+   alert(event.target.tagName); 
+ });
+
+ // ...dispatch on elem!
+ let event = new Event("goodbye", {bubbles: true}); // 
+ ElemId.dispatchEvent(event);
+
+
+ //
 document.body.addEventListener("click", function(e){
    console.log(`positions: ${e.clientX} ${e.clientY}`);
 })
